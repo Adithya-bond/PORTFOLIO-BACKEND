53e642fd95780aef239b77a5e8d67094e228f443
Adithya kC <adithyamh@gmail.com>
2025-02-21 14:13:38 +0530

    updated the json server version cuz it was not working with beta version

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const jsonServer=require("json-server");
-const server=jsonServer.create();
-const router=jsonServer.router("data.json");
-const middlewares=jsonServer.defaults();
-const port=process.env.PORT || 8080;
+const jsonServer =require('json-server')
+const server = jsonServer.create();
+const router = jsonServer.router("./data.json"); // Ensure db.json exists in the same directory
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(port)
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
